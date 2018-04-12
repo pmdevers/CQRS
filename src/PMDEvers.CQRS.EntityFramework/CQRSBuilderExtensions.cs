38f9b9b97d145fe78c0c10db89b75e23e95626ca
Patrick Evers <p.evers@tjip.com>
2018-04-12 16:51:11 +0200

    Update Stores

--- src/PMDEvers.CQRS.EntityFramework/CQRSBuilderExtensions.cs
+++ src/PMDEvers.CQRS.EntityFramework/CQRSBuilderExtensions.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using System.Text;
 
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace PMDEvers.CQRS.EntityFramework
 {
     public static class CQRSBuilderExtensions
     {
-        public static CQRSBuilder AddEntityFramework(this CQRSBuilder builder)
+        public static CQRSBuilder AddEntityFramework(this CQRSBuilder builder, Action<DbContextOptionsBuilder> options = null)
         {
-            builder.Services.AddDbContext<EventContext>();
+            builder.Services.AddDbContext<EventContext>(options);
             return builder.AddEventStore<EventStore>();
         }
     }
--- src/PMDEvers.CQRS.InMemory/CQRSBuilderExtensions.cs
+++ src/PMDEvers.CQRS.InMemory/CQRSBuilderExtensions.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using System.Text;
 
+using Microsoft.Extensions.DependencyInjection;
+
+using PMDEvers.CQRS.Interfaces;
+
 namespace PMDEvers.CQRS.InMemory
 {
     public static class CQRSBuilderExtensions
     {
         public static CQRSBuilder AddInMemoryEventStore(this CQRSBuilder builder)
         {
-            return builder.AddEventStore<EventStore>();
+            builder.Services.AddSingleton<IEventStore, EventStore>();
+            return builder;
         }
     }
 }
