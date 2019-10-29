31aa96161d9e23433ac280bc4094e5335e16a6d3
Patrick Evers <pmdevers@gmail.com>
2019-10-29 14:17:51 +0100

    Allow Adding CommandHandlers with return values

---
+++ src/PMDEvers.CQRS/Builder/CQRSBuilderExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using PMDEvers.Servicebus;
+
+namespace PMDEvers.CQRS
+{
+    public static class CQRSBuilderExtensions
+    {
+        public static CQRSBuilder AddCommandHandler<TCommand, TReturn, TCommandHandler>(this CQRSBuilder builder)
+            where TCommand : ICommand<TReturn>
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (typeof(ICommandHandler<TCommand, TReturn>).IsAssignableFrom(typeof(TCommandHandler)))
+            {
+                builder.Services.AddScoped(typeof(ICommandHandler<TCommand, TReturn>), typeof(TCommandHandler));
+            }
+
+            if (typeof(IAsyncCommandHandler<TCommand, TReturn>).IsAssignableFrom(typeof(TCommandHandler)))
+            {
+                builder.Services.AddScoped(typeof(IAsyncCommandHandler<TCommand, TReturn>), typeof(TCommandHandler));
+            }
+
+            if (typeof(ICancellableAsyncCommandHandler<TCommand, TReturn>).IsAssignableFrom(typeof(TCommandHandler)))
+            {
+                builder.Services.AddScoped(typeof(ICancellableAsyncCommandHandler<TCommand, TReturn>), typeof(TCommandHandler));
+            }
+
+            return builder;
+        }
+    }
+}
