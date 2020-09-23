148bc451a4ac19243864a08b805a85c57e6a03ad
Patrick Evers <pmdevers@hotmail.com>
2020-09-23 14:46:56 +0200

    Update net version

--- sample/PMDEvers.CQRS.Sample/Startup.cs
+++ sample/PMDEvers.CQRS.Sample/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-
+using Microsoft.Extensions.Hosting;
 using PMDEvers.CQRS.Builder;
 using PMDEvers.CQRS.Events;
 using PMDEvers.CQRS.InMemory;
@@ -44,7 +44,7 @@ namespace PMDEvers.CQRS.Sample
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
